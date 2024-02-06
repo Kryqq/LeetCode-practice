ac5ccc657dd8e7df8e34ca83d204b5643acc7a40
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-02-06 19:23:50 +0700

    gropupAnagram

---
+++ firstUnique/387.js
@@ -0,0 +1,15 @@
+const firstUniqChar = function (s) {
+   const table = {};
+
+   for (const char of s) {
+      table[char] = table[char] ? table[char] + 1 : 1;
+   }
+   for (let i = 0; i < s.length; i++) {
+      if (table[s[i]] === 1) {
+         return i;
+      }
+   }
+   return -1;
+};
+
+console.log(firstUniqChar('loveleetcode'));
